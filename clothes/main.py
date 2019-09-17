222d202042508df953b70348c58e9b8e34efb2f8
vagrant <vagrant@vagrant.vm>
2019-09-17 07:50:47 +0000

    ENH: Adding image classification

---
+++ clothes/main.py
@@ -0,0 +1,64 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+# Followed tutorial
+# URL: https://www.tensorflow.org/tutorials/keras/basic_classification
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+# Load dataset
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Labels 
+# 0	T-shirt/top
+# 1	Trouser
+# 2	Pullover
+# 3	Dress
+# 4	Coat
+# 5	Sandal
+# 6	Shirt
+# 7	Sneaker
+# 8	Bag
+# 9	Ankle boot
+
+# Normalize images
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# Setup layers
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(train_images, train_labels, epochs=5)
+
+# Evaluate accuracy
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
+# Make a prediction
+predictions = model.predict(test_images)
+prediction = np.argmax(predictions[0])
+
+print(prediction)
+
